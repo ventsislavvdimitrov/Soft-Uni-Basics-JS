bf08c72e376916a0d010908affa43f771d432725
Ventsislav Dimitrov <104695140+ventsislavvdimitrov@users.noreply.github.com>
2022-10-11 23:24:22 +0300

    Add files via upload

---
+++ forLoopLab/sumOfNumbers.js
@@ -0,0 +1,16 @@
+function sumOfNumbers(input) {
+
+    let numberAsString = String(input[0]);
+
+    let sum = 0;
+
+    for (let i = 0; i < numberAsString.length; i++) {
+        let number = Number(numberAsString[i]);
+        sum += number;
+    }
+
+    console.log(`The sum of the digits is:${sum}`);
+
+}
+
+sumOfNumbers(['1234']);
