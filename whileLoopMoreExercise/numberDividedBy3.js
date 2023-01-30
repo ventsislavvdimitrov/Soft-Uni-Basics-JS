de5673d54124e940f5cbd74dbdb27bf62cd644c4
Ventsislav Dimitrov <104695140+ventsislavvdimitrov@users.noreply.github.com>
2023-01-30 23:17:35 +0200

    Add files via upload

---
+++ whileLoopMoreExercise/numberDividedBy3.js
@@ -0,0 +1,11 @@
+function numbersDividedBy3WithoutReminder() {
+
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
+
+}
+
+numbersDividedBy3WithoutReminder();
